30b44f1c34add50abe7c4c362212b58557c2e409
shahram <shahram@newrelic.com>
2021-04-26 15:29:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# nri-bosh-release-windows
+A bosh release for deploying the New Relic Infrastructure agent on Windows
+
+This is a self contained bosh release for installing the New Relic Infrastructure agent. It includes the deb package as a blob and has an install script that installs the package. The package is the install package for Windows.
+
+The release is meant to be installed as an addon. In order to facilitate that, a sample runtime.yml file is included.
+*** Please remember to update the license_key to your license key and double check version number.
+
+To use, upload the release:
+```bash
+bosh upload-release ~/Downloads/nr-bosh-release-windows-1.0.1033.tgz
+```
+
+and. Then update the runtime config
+```bash
+bosh update-runtime-config runtime.yml
+```
+
+Then redeploy to pick up the addon.
---
+++ jobs/install-nri/templates/nri.ps1
@@ -0,0 +1,60 @@
+
+Write-Host "Installing New Relic Infrastructure"
+
+$DataStamp = get-date -Format yyyyMMddTHHmmss
+$logFile = 'C:\var\vcap\sys\log\install-nri\install-nri.log'
+
+Write-Host $DataStamp "`r`n"
+
+$arrService = Get-Service -Name "newrelic-infra" -ErrorAction SilentlyContinue
+if ($arrService) {
+  Write-Host "New Relic Inrastructure already installed."
+  if ($arrService.Status -eq 'Running') {
+    Write-Host "Stopping New Relic Infrastructure"
+    Stop-Service "newrelic-infra"
+    Start-Sleep -seconds 30
+    $arrService.Refresh()
+    if ($arrService.Status -eq 'Running') {
+      Write-Host "New Relic Infrastructure did not stop"
+    }
+  }
+}
+
+$MSIArguments = @(
+    "/qn"
+    "/i"
+    "C:\var\vcap\packages\nr-infra\newrelic-infra.1.2.1.msi"
+    "/L*v"
+    $logFile
+)
+
+Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow
+
+Write-Host "Installer done. `r`n"
+
+$configFile = "C:\Program Files\New Relic\newrelic-infra\newrelic-infra.yml"
+
+Set-Content -Path $configFile -Value "license_key: <%= p('infra_agent.license_key') %>`r`n"
+
+Add-Content -Path $configFile -Value "display_name: <%= spec.name + '-' + spec.id %>`r`n"
+Add-Content -Path $configFile -Value "log_file: C:\var\vcap\data\sys\log\install-nri\newrelic-infa.log`r`n"
+
+Add-Content -Path $configFile -Value "custom_attributes:"
+Add-Content -Path $configFile -Value "  bosh.az: <%= spec.az %>"
+Add-Content -Path $configFile -Value "  bosh.bootstrap: <%= spec.bootstrap %>"
+Add-Content -Path $configFile -Value "  bosh.deployment: <%= spec.deployment %>"
+Add-Content -Path $configFile -Value "  bosh.id: <%= spec.id %>"
+Add-Content -Path $configFile -Value "  bosh.index: <%= spec.index %>"
+Add-Content -Path $configFile -Value "  bosh.ip: <%= spec.ip %>"
+Add-Content -Path $configFile -Value "  bosh.name: <%= spec.name %>"
+Add-Content -Path $configFile -Value "  bosh.environment: <%= p('infra_agent.environment') %>"
+
+<%- if_p('infra_agent.custom_attributes') do |cas| -%>
+  <%- cas.each do |key, value| -%>
+     <%- unless value.empty? -%>
+Add-Content -Path $configFile -Value "  bosh.<%= key %>: <%= value %>"
+     <%- end -%>
+  <%- end -%>
+<%- end -%>
+
+Start-Service "newrelic-infra"
